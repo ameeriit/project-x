230ac8991a522623ce466cb17507cf1bc7e03875
@meeriit <amritpokharel51@gmail.com>
2022-12-21 17:41:29 +0545

    major section works done

--- assests/js/common/main.js
+++ assests/js/common/main.js
@@ -6,3 +6,27 @@ jQuery(function ($) {
         $('.menu-list').toggleClass('expand');
     });
 });
+
+
+
+$('.brands-wrapper').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+});
+
+
+$(function () {
+	$('.popup-modal').magnificPopup({
+		type: 'inline',
+		preloader: false,
+		focus: '#username',
+		modal: true
+	});
+	$(document).on('click', '.popup-modal-dismiss', function (e) {
+		e.preventDefault();
+		$.magnificPopup.close();
+	});
+});
