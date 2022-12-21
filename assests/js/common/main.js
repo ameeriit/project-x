929b4e9b715b54f7156e9c115845e3c6efc41465
@meeriit <amritpokharel51@gmail.com>
2022-12-22 01:30:35 +0545

    minor card adjustments

--- assests/js/common/main.js
+++ assests/js/common/main.js
@@ -1,32 +1,27 @@
 /* hamburger menu */
 
-jQuery(function ($) {
-    $('.menu-btn').click(function () {
-        $(this).toggleClass('open');
-        $('.menu-list').toggleClass('expand');
-    });
+// jQuery(function ($) {
+//     $('.menu-btn').click(function () {
+//         $(this).toggleClass('open');
+//         $('.menu-list').toggleClass('expand');
+//     });
+// });
+
+/* slick for brands slider brand section*/
+
+$(".brands-wrapper").slick({
+  slidesToShow: 6,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows: false,
 });
 
+/* swiper for cards slider service section */
 
 
-$('.brands-wrapper').slick({
-    slidesToShow: 6,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    arrows: false,
-});
 
-
-$(function () {
-	$('.popup-modal').magnificPopup({
-		type: 'inline',
-		preloader: false,
-		focus: '#username',
-		modal: true
-	});
-	$(document).on('click', '.popup-modal-dismiss', function (e) {
-		e.preventDefault();
-		$.magnificPopup.close();
-	});
+var swiper = new Swiper(".swiper-container", {
+  slidesPerView: 3,
+  spaceBetween: 30,
 });
