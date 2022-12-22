ac3746be7b3238af8cc290b295fa79f6cfbb92ba
@meeriit <amritpokharel51@gmail.com>
2022-12-22 17:35:35 +0545

    major issues overcome

--- assests/js/common/main.js
+++ assests/js/common/main.js
@@ -15,6 +15,34 @@ $(".brands-wrapper").slick({
   autoplay: true,
   autoplaySpeed: 2000,
   arrows: false,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
 });
 
 /* swiper for cards slider service section */
@@ -24,6 +52,7 @@ $(".brands-wrapper").slick({
 var swiper = new Swiper(".swiper-container", {
   slidesPerView: 3,
   spaceBetween: 30,
+  
 });
 
 
@@ -39,4 +68,63 @@ $(document).ready(function() {
 
 		fixedContentPos: false
 	});
+});
+
+/* sync slider */
+
+$('.slider-single').slick({
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	arrows: false,
+	fade: false,
+	adaptiveHeight: true,
+	infinite: false,
+   useTransform: true,
+	speed: 400,
+	cssEase: 'cubic-bezier(0.77, 0, 0.18, 1)',
+});
+
+$('.slider-nav')
+	.on('init', function(event, slick) {
+		$('.slider-nav .slick-slide.slick-current').addClass('is-active');
+	})
+	.slick({
+		slidesToShow: 7,
+		slidesToScroll: 7,
+		dots: false,
+		focusOnSelect: false,
+		infinite: false,
+		responsive: [{
+			breakpoint: 1024,
+			settings: {
+				slidesToShow: 5,
+				slidesToScroll: 5,
+			}
+		}, {
+			breakpoint: 640,
+			settings: {
+				slidesToShow: 4,
+				slidesToScroll: 4,
+		   }
+		}, {
+			breakpoint: 420,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 3,
+	   }
+		}]
+	});
+
+$('.slider-single').on('afterChange', function(event, slick, currentSlide) {
+	$('.slider-nav').slick('slickGoTo', currentSlide);
+	var currrentNavSlideElem = '.slider-nav .slick-slide[data-slick-index="' + currentSlide + '"]';
+	$('.slider-nav .slick-slide.is-active').removeClass('is-active');
+	$(currrentNavSlideElem).addClass('is-active');
+});
+
+$('.slider-nav').on('click', '.slick-slide', function(event) {
+	event.preventDefault();
+	var goToSingleSlide = $(this).data('slick-index');
+
+	$('.slider-single').slick('slickGoTo', goToSingleSlide);
 });
